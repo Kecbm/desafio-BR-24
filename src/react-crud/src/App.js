39047192d6fa703ab28b21e6d9461d8de52b4dc1
Klecianny Melo <kleciannymelo@gmail.com>
2023-03-30 23:46:32 -0300

    :sparkles: feat: Iniciando a construção da aplicação React

--- src/react-crud/src/App.js
+++ src/react-crud/src/App.js
@@ -1,9 +1,29 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import ListUser from './components/ListUser';
+import CreateUser from './components/CreateUser';
+import EditUser from './components/EditUser';
 
 function App() {
   return (
     <div>
-      <h1>Hello, World</h1>
+      <h1>React CRUD using PHP and MySQL</h1>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">List Users</Link>
+            </li>
+            <li>
+              <Link to="user/create">Create Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<ListUser />} />
+          <Route patch="user/create" element={<CreateUser />} />
+          <Route patch="user/:id/edit" element={<EditUser />} />
+        </Routes>
     </div>
   );
 }

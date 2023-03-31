7ae042962c885ce9d81d3b1638eaac677dc114f0
Klecianny Melo <kleciannymelo@gmail.com>
2023-03-31 11:36:37 -0300

    :sparkles: feat: Formulário em ListUser

--- src/react-crud/src/components/ListUser.js
+++ src/react-crud/src/components/ListUser.js
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 export default function ListUser() {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(inputs);
+    }
+
     return (
-        <h1>List Users</h1>
+        <div>
+            <h1>List Users</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Name: </label>
+                <input type="text" name="name" onChange={handleChange} />
+                <br />
+                <label>Email: </label>
+                <input type="text" name="email" onChange={handleChange} />
+                <br />
+                <label>Mobile: </label>
+                <input type="text" name="mobile" onChange={handleChange} />
+                <br />
+                <button>Save</button>
+            </form>
+        </div>
     )
 }

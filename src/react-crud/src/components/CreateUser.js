33fb04d13376ac7dd1b0766fab418f93b654b728
Klecianny Melo <kleciannymelo@gmail.com>
2023-04-03 18:17:25 -0300

    :sparkles: feat: estutura de CreateUser em React

--- src/react-crud/src/components/CreateUser.js
+++ src/react-crud/src/components/CreateUser.js
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default function CreateUser() {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.post('http://localhost:8000/api/user/save', inputs);
+        console.log(inputs);
+    }
+
     return (
-        <h1>Create User</h1>
+        <div>
+            <h1>Create User</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Name: </label>
+                <input type="text" name="name" onChange={handleChange} />
+                <br />
+                <label>Email: </label>
+                <input type="text" name="email" onChange={handleChange} />
+                <br />
+                <label>Mobile: </label>
+                <input type="text" name="mobile" onChange={handleChange} />
+                <br />
+                <button>Save</button>
+            </form>
+        </div>
     )
 }

7d19978914b6ecc90a99658ad289f9c745475d38
Klecianny Melo <kleciannymelo@gmail.com>
2023-04-05 00:03:34 -0300

    :sparkles: feat: Ajustando o App.js

--- src/react-crud/src/App.js
+++ src/react-crud/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import './App.css';
+import './css/App.css';
 import ListUser from './components/ListUser';
 import CreateUser from './components/CreateUser';
 import EditUser from './components/EditUser';
@@ -7,23 +7,17 @@ import EditUser from './components/EditUser';
 function App() {
   return (
     <div>
-      <h1>React CRUD using PHP and MySQL</h1>
-
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">List Users</Link>
-            </li>
-            <li>
-              <Link to="user/create">Create Users</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route index element={<ListUser />} />
-          <Route path="user/create" element={<CreateUser />} />
-          <Route path="user/:id/edit" element={<EditUser />} />
-        </Routes>
+      <h1 className="title title-blue">⚡ My Business Manager 24</h1>
+      <h1 className="title title-black">A plataforma de gestão empresarial que nasceu para revolucionar o mundo</h1>
+      <p className="text">O único software necessário para garantir a segurança e o sucesso do seu negócio!</p>
+      <button className="button first-button">Entre em contato</button>
+      <button className="button second-button"><Link to="/" className="link">Lista de Usuários</Link></button>
+      <button className="button second-button"><Link to="user/create" className="link">Criar Usuários</Link></button>
+      <Routes>
+        <Route index element={<ListUser />} />
+        <Route path="user/create" element={<CreateUser />} />
+        <Route path="user/:id/edit" element={<EditUser />} />
+      </Routes>
     </div>
   );
 }

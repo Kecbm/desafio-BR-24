6d5de951dbd72ab288fd5f60e5a84091ff708b36
Klecianny Melo <kleciannymelo@gmail.com>
2023-04-05 12:09:48 -0300

    :hammer: refactor: Adequando o método POST para Company

--- src/api/index.php
+++ src/api/index.php
@@ -31,13 +31,19 @@ switch($method) {
         break;
 
     case "POST":
-        $user = json_decode( file_get_contents('php://input') );
-        $sql = "INSERT INTO company(id, name, email, mobile, created_at) VALUES (null, :name, :email, :mobile, :created_at)";
+        $company = json_decode( file_get_contents('php://input') );
+        $sql = "INSERT INTO company(id, name, email, CNPJ, corporate_name, annual_recipe, name_first_contact, last_name_first_contact, name_second_contact, last_name_second_contact, created_at) VALUES (null, :name, :email, :CNPJ, :corporate_name, :annual_recipe, :name_first_contact, :last_name_first_contact, :name_second_contact, :last_name_second_contact, :created_at)";
         $stmt = $conn->prepare($sql);
         $created_at = date('Y-m-d');
-        $stmt->bindParam(':name', $user->name);
-        $stmt->bindParam(':email', $user->email);
-        $stmt->bindParam(':mobile', $user->mobile);
+        $stmt->bindParam(':name', $company->name);
+        $stmt->bindParam(':email', $company->email);
+        $stmt->bindParam(':CNPJ', $company->CNPJ);
+        $stmt->bindParam(':corporate_name', $company->corporate_name);
+        $stmt->bindParam(':annual_recipe', $company->annual_recipe);
+        $stmt->bindParam(':name_first_contact', $company->name_first_contact);
+        $stmt->bindParam(':last_name_first_contact', $company->last_name_first_contact);
+        $stmt->bindParam(':name_second_contact', $company->name_second_contact);
+        $stmt->bindParam(':last_name_second_contact', $company->last_name_second_contact);
         $stmt->bindParam(':created_at', $created_at);
 
         if($stmt->execute()) {

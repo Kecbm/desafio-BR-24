034c8196a8711ddde36c9283f6c5560fb92f35d9
Klecianny Melo <kleciannymelo@gmail.com>
2023-04-05 15:37:02 -0300

    :sparkles: feat: ajuste na estrutura do App.js

--- src/react-crud/src/App.js
+++ src/react-crud/src/App.js
@@ -6,18 +6,22 @@ import EditCompany from './components/EditCompany';
 
 function App() {
   return (
-    <div>
-      <h1 className="title title-blue">⚡ My Business Manager 24</h1>
-      <h1 className="title title-black">A plataforma de gestão empresarial que nasceu para revolucionar o mundo</h1>
-      <p className="text">O único software necessário para garantir a segurança e o sucesso do seu negócio!</p>
-      <button className="button first-button">Entre em contato</button>
-      <button className="button second-button"><Link to="/" className="link">Lista de Empresas</Link></button>
-      <button className="button second-button"><Link to="company/create" className="link">Criar Empresa</Link></button>
-      <Routes>
-        <Route index element={<ListCompany />} />
-        <Route path="company/create" element={<CreateCompany />} />
-        <Route path="company/:id/edit" element={<EditCompany />} />
-      </Routes>
+    <div id="home-page">
+      <h1 className="title title-blue">⚡MBM24</h1>
+      <section id="content-home">
+        <h1 className="title title-blue">⚡ My Business Manager 24</h1>
+        <h1 className="title title-black">A plataforma de gestão empresarial que nasceu para revolucionar o mundo</h1>
+        <p className="text">O único software necessário para garantir a segurança e o sucesso do seu negócio!</p>
+      </section>
+      <section id="storage-company">
+        <button className="button first-button"><Link to="/" className="link">Lista de Empresas</Link></button>
+        <button className="button first-button"><Link to="company/create" className="link">Criar Empresa</Link></button>
+        <Routes>
+          <Route index element={<ListCompany />} />
+          <Route path="company/create" element={<CreateCompany />} />
+          <Route path="company/:id/edit" element={<EditCompany />} />
+        </Routes>
+      </section>
     </div>
   );
 }

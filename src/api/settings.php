b8169546b435a63e46d9d15601ec0f855b1ffe47
Klecianny Melo <kleciannymelo@gmail.com>
2023-04-06 11:29:50 -0300

    :sparkles: feat: criação do arquivo settings.php

---
+++ src/api/settings.php
@@ -0,0 +1,11 @@
+<?php
+define('C_REST_CLIENT_ID','local.6425cbaf921bc7.91713194');//Application ID
+define('C_REST_CLIENT_SECRET','l9gr6K6xPyoOxHvz7eJsoYWu45Oy4q8aum1ty2HcMsP9F92jc1');//Application key
+// or
+// define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/');//url on creat Webhook
+
+// define('C_REST_CURRENT_ENCODING','windows-1251');
+// define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
+// define('C_REST_LOG_TYPE_DUMP',true); //logs save var_export for viewing convenience
+// define('C_REST_BLOCK_LOG',true);//turn off default logs
+// define('C_REST_LOGS_DIR', __DIR__ .'/logs/'); //directory path to save the log

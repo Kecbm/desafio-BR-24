db44a47fe9a288739345b4ee79132197f12deecc
Klecianny Melo <kleciannymelo@gmail.com>
2023-04-06 14:20:50 -0300

    :sparkles: feat: URL do Web Hook

--- src/api/settings.php
+++ src/api/settings.php
@@ -4,7 +4,7 @@ define('C_REST_CLIENT_SECRET','l9gr6K6xPyoOxHvz7eJsoYWu45Oy4q8aum1ty2HcMsP9F92jc
 //  or
 define('C_REST_WEB_HOOK_URL','https://b24-56y3mh.bitrix24.com.br/rest/1/lhzkrvtq0tzlddw1/');//url on creat Webhook
 
-// define('C_REST_CURRENT_ENCODING','windows-1251');
+// define('C_REST_CURRENT_ENCODING','windows-1251');j
 // define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
 // define('C_REST_LOG_TYPE_DUMP',true); //logs save var_export for viewing convenience
 // define('C_REST_BLOCK_LOG',true);//turn off default logs
